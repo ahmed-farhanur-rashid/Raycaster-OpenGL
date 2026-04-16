57ef9588f9e78ebdd9f42bb4621e1ebbee73ea62
ahmed-farhanur-rashid <ahmed.farhanur.rashid@gmail.com>
2026-04-16 10:49:03 +0600

    Changed comments.

--- src/projectile/projectile.h
+++ src/projectile/projectile.h
@@ -24,7 +24,7 @@ struct Projectile {
 };
 
 /* ================================================================== */
-/*  projectile namespace — global state                                */
+/*  projectile namespace — global state                               */
 /* ================================================================== */
 
 namespace projectile {
